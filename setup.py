ecccce73777b5054017dfa6189842f6830eb2df5
andydevs <akanshul97@gmail.com>
2018-10-21 17:29:10 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""
+Tketris
+
+Tetris using tkinter
+
+Author:  Anshul Kharbanda
+Created: 10 - 11 - 2018
+"""
+from distutils.core import setup
+
+# Setup function
+setup(name='tketris',
+      version='1.0',
+      description='Tetris written in python and tkinter',
+      author='Anshul Kharbanda',
+      author_email='akanshul97@gmail.com',
+      homepage='http://github.com/andydevs/tketris',
+      packages=[
+        'tketris',
+        'tketris.game',
+        'tketris.view',
+        'tketris.view.side_menu'
+      ])

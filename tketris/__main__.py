08928d25abb76e52eb11a87231d286cd955ea43d
andydevs <akanshul97@gmail.com>
2018-10-27 21:17:46 -0400

    Added debug arguments

--- tketris/__main__.py
+++ tketris/__main__.py
@@ -8,12 +8,20 @@ Created: 10 - 11 - 2018
 """
 from . import Tketris
 from tkinter import Tk
+from argparse import ArgumentParser
+from sys import argv
+
+# Argument Parser
+parser = ArgumentParser()
+parser.add_argument('-d', '--debug', action='store_true')
 
 # Main code
 if __name__ == '__main__':
+    args = parser.parse_args(argv[1:])
     root = Tk()
     app = Tketris(root)
     app.pack()
+    app.DEBUG = args.debug
     root.title('Tketris')
     root.resizable(False, False)
     root.mainloop()
